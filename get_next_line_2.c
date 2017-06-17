bfc43708b0a02ed907e6cd1ba402e005ad2489e9
Louis LORGERE <llorgere@e1r4p5.42.fr>
2017-06-17 17:48:01 +0200

    faut bien continuer un jour

--- get_next_line_2.c
+++ get_next_line_2.c
@@ -6,11 +6,12 @@
 /*   By: llorgere <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/17 14:49:32 by llorgere          #+#    #+#             */
-/*   Updated: 2017/06/17 17:40:52 by llorgere         ###   ########.fr       */
+/*   Updated: 2017/06/17 17:47:54 by llorgere         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
 
 static char	*ft_buff_cpy(int i, char *buff)
 {
@@ -32,41 +33,51 @@ static char	*ft_buff_cpy(int i, char *buff)
 int			get_next_line(const int fd, char **line)
 {
 	static char		*tmp = NULL;
-//	char			*r_line;
-//	int				nb_read;
+	//	char			*r_line;
+	//	int				nb_read;
 	int				i;
 	char 			buff[BUFF_SIZE];
 
-//	nb_read = 1;
-//	free(&(line));
+	//	nb_read = 1;
+	//	free(&(line));
+	printf("test 1\n");
 	while (read(fd, buff, BUFF_SIZE))
 	{
+		printf("test 2\n");
 		i = 0;
 		while (i <  BUFF_SIZE)
 		{
+			printf("test 3\n");
 			if (buff[i] == '\n')
 			{
-// copie buff jusqu'a buff[i - 1] dans line
+				printf("test 4\n");
+				// copie buff jusqu'a buff[i - 1] dans line
 				*line = (char *)malloc(sizeof(char) * (ft_strlen(tmp) + ft_strlen(buff) + 1));
+				printf("test du malloc de line\n");
 				*line = ft_strjoin(tmp, ft_buff_cpy(i, buff));
 				free(tmp);
 				free(ft_buff_cpy(i, buff));
 				tmp = ft_strdup(buff + i);
 				return (1);
 			}
+	//		printf("test 5\n");
 			else if (buff[i] == '\0')
 			{
+				printf("test 6\n");
 				*line = (char *)malloc(sizeof(char) * (ft_strlen(tmp) + ft_strlen(buff) + 1));
 				*line = ft_strjoin(tmp, ft_buff_cpy(i, buff));
 				free(ft_buff_cpy(i, buff));
 				free(tmp);
 				return (1);
 			}
+			printf("test 7\n");
 			i++;
 		}
-//		tmp = (char*)malloc(sizeof(char) * BUFF_SIZE * nb_read);
+		printf("test 8\n");
+		//		tmp = (char*)malloc(sizeof(char) * BUFF_SIZE * nb_read);
 		tmp = ft_strjoin(tmp, buff);
-//		nb_read++;
+		//		nb_read++;
 	}
+	printf("test 9\n");
 	return (0);
 }
